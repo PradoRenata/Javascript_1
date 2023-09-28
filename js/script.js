c5e3f5b4b51d4635eb762aff4bd293d60fc47788
Renata Prado <pradov.renata@gmail.com>
2023-09-27 22:35:38 -0300

    Subida de la Pre-entrega 1

--- js/script.js
+++ js/script.js
@@ -1,13 +1,64 @@
-let savedUser ="RonaRamona"
+let savedUser = "UserTest";
+let savedPass = "Secret123";
 
-for (let i = 1; i <=3 ; i++) {
-    let enterUser = prompt ("Ingresa tu nombre de usuario");
+alert("Bienvenido a un simulador para gestionar tus lecturas pendientes.")
 
-    if (savedUser === enterUser){
-        alert("Bienvenida de vuelta "+savedUser)
-        break;
-    }else{
-        alert("El usuario es incorrecto, intento "+i+" de 3");
-    }    
+function login(){
+    let access =false;
+
+    //No se si habrá una mejor forma de hacer esto, pero cabeceándome harto salió. xD
+    let userName = prompt ("Ingresa tu nombre de usuario.");
+    while (userName != savedUser){
+        alert("No es un usuario registrado.");
+        userName = prompt ("Ingresa tu nombre de usuario.");
+    }
+
+    let attempts = 3;
+    for (let i = attempts ; i > 0 ; i--) {
+        let userPass = prompt ("Ingresa tu contraseña para ingresar. Tienes "+i+" intentos.")
+        if (userPass===savedPass){
+            alert("ingreso exitoso.");
+            access = true;
+            break;
+        }else{
+            alert("Contraseña incorrecta, te quedan "+(i-1)+" intentos.")
+        }
+    }
+    return access;
 }
-alert("Fin")
+
+if (login()) {
+    //Si pudimos acceder
+    let bookList = "\n- Cien años de soledad. \n- Tengo miedo torero. \n- Solanin. "
+    let options = prompt ("Elige una opción: \n1 - Ver listado de libros. \n2 - Agregar un Libro. \n3 - Cerrar sesión.")
+
+    while (options != "3"){
+        switch (options){
+            case '1':
+                alert("Tu lista de libros es: "+ bookList)
+                break;
+            
+            case '2':
+                let addBook = prompt ("Ingresa el titulo del libro que quieres agregar a tu lista.")
+                //bookList += addBook (Esto fue un intento fallido pero lo dejo de todas formas jajaja)
+                //Acá no pude hacer que se agreguen como una lista, pero luego de la clase de arrays pienso que se puede utilizar esa forma para gestionar los titulos de la lista.
+                alert ("Ingresaste un titulo exitosamente.\nTu nueva lista es: "+bookList+"\n- "+addBook)
+                break;
+                //Por ahora lo dejo para que se sobre escriba en cada ciclo. Pero a la siguiente puedo trabajar en gestionar los titulos, y así poder quitarlos de la lista también.
+
+            default:
+                alert('Elegiste una opción inválida');
+                break;
+        }
+
+        options = prompt ("Elige una opción: \n1 - Ver listado de libros. \n2 - Agregar un Libro. \n3 - Cerrar sesión.");
+    }
+
+}else{
+    //Si No pudo acceder
+    alert("Cuenta bloqueada temporalmente, intenta de nuevo más tarde.")
+}
+
+alert("Vuelve pronto!")
+
+
